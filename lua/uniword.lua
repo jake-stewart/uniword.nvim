5a553cb078d458afefda6128901d8ba17f94fa4a
Jake Stewart <jakey@snakey.local>
2023-09-15 00:05:43 +0800

    fix bugs

--- lua/uniword.lua
+++ lua/uniword.lua
@@ -1,16 +1,19 @@
 REGEX = [[\v(<@=(\i@!\k)+|<@=\k+|(\k@!\S)+)]];
 
 local function createMappings()
-    local function map(key, flags, regex)
-        vim.keymap.set({"n", "o", "v"}, key, function()
-            vim.fn.search(regex, flags)
-        end)
+    local function callback(flags, regex)
+        return function()
+            vim.fn.search(regex, "W" .. flags)
+        end
     end
 
-    map("w", "", REGEX .. "|^")
-    map("b", "b", REGEX .. "|^")
-    map("ge", "be", REGEX .. "|^")
-    map("e", "e", REGEX)
+    vim.keymap.set({"n", "v"}, "w", callback("", REGEX .. "|^"))
+    vim.keymap.set({"n", "v"}, "b", callback("b", REGEX .. "|^"))
+    vim.keymap.set({"n", "v", "o"}, "ge", callback("be", REGEX .. "|^"))
+    vim.keymap.set({"n", "v"}, "e", callback("e", REGEX))
+    vim.keymap.set("o", "w", callback("e", REGEX .. ".|^"))
+    vim.keymap.set("o", "e", callback("e", REGEX .. "."))
+    vim.keymap.set("o", "b", callback("b", REGEX .. "|^"))
 end
 
 local function createTextObject()
