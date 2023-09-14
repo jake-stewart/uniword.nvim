aba76316f1ea0a5ce14ffb989858466f47350c38
Jake Stewart <jakey@snakey.local>
2023-09-15 00:14:44 +0800

    add count support

--- lua/uniword.lua
+++ lua/uniword.lua
@@ -3,7 +3,10 @@ REGEX = [[\v(<@=(\i@!\k)+|<@=\k+|(\k@!\S)+)]];
 local function createMappings()
     local function callback(flags, regex)
         return function()
-            vim.fn.search(regex, "W" .. flags)
+            local count = vim.v.count or 1
+            for i = 1, count + 1 do
+                vim.fn.search(regex, "W" .. flags)
+            end
         end
     end
 
@@ -11,7 +14,8 @@ local function createMappings()
     vim.keymap.set({"n", "v"}, "b", callback("b", REGEX .. "|^"))
     vim.keymap.set({"n", "v", "o"}, "ge", callback("be", REGEX .. "|^"))
     vim.keymap.set({"n", "v"}, "e", callback("e", REGEX))
-    vim.keymap.set("o", "w", callback("e", REGEX .. ".|^"))
+    vim.keymap.set("o", "w", callback("w", REGEX .. "|^"))
+    vim.keymap.set("n", "cw", callback("ce", REGEX .. ".|^"), {remap = true})
     vim.keymap.set("o", "e", callback("e", REGEX .. "."))
     vim.keymap.set("o", "b", callback("b", REGEX .. "|^"))
 end
