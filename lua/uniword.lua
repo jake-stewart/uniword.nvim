bd515e8f138a87fc65e3edd045a27dda39d8980a
Jake Stewart <jakey@snakey.local>
2023-09-15 00:15:41 +0800

    fix change word mapping

--- lua/uniword.lua
+++ lua/uniword.lua
@@ -15,9 +15,9 @@ local function createMappings()
     vim.keymap.set({"n", "v", "o"}, "ge", callback("be", REGEX .. "|^"))
     vim.keymap.set({"n", "v"}, "e", callback("e", REGEX))
     vim.keymap.set("o", "w", callback("w", REGEX .. "|^"))
-    vim.keymap.set("n", "cw", callback("ce", REGEX .. ".|^"), {remap = true})
     vim.keymap.set("o", "e", callback("e", REGEX .. "."))
     vim.keymap.set("o", "b", callback("b", REGEX .. "|^"))
+    vim.keymap.set("n", "cw", "ce", {remap = true})
 end
 
 local function createTextObject()
